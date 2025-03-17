131fd8a9a14f9d5176d44c60084e6db55075780a
mushin26 <mushin26@gmail.com>
2025-03-17 18:58:36 +0000

    backing up work

---
+++ fix_string-2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Define the string
+quote = "Live Long And Prosper"
+
+# Chain methods to lower the string, split it, and join with "-"
+# result = quote.lower().split()
+result1 = "-".join(quote.lower().split()) 
+print(result1)
---
+++ fix_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+# Transform the string
+title = "star trek"
+print(title.capitalize())
